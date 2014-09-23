ad741c8716feb02dfda51c0264aba483f2657e47
cfc1020 <cfc9209@gmail.com>
2014-09-23 16:03:03 +0300

    Initial Commit

---
+++ SimpleWeather/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SimpleWeather
+//
+//  Created by Andrey on 23.09.14.
+//  Copyright (c) 2014 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
